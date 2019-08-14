8ce1cd6310bf03101575942fe25aae9a0c430770
Dulce Perez <dulcefrancelispc@gmail.com>
2019-08-14 10:48:12 -0400

    adding slideshow

--- js/main.js
+++ js/main.js
@@ -7,4 +7,35 @@ $(window).on('scroll', function() {
         $(".main-nav").removeClass("active");
         $('.main-nav').addClass('fixed');
     }
+});
+
+var $carousel = $('.homeslide').flickity({
+    prevNextButtons: false,
+    pageDots: false,
+    cellAlign: 'left'
+});
+// Flickity instance
+    var flkty = $carousel.data('flickity');
+  // elements
+  var $cellButtonGroup = $('.button-group--cells');
+  var $cellButtons = $cellButtonGroup.find('.button');
+
+  // update selected cellButtons
+  $carousel.on('select.flickity', function () {
+      $cellButtons.filter('.is-selected')
+          .removeClass('is-selected');
+      $cellButtons.eq(flkty.selectedIndex)
+          .addClass('is-selected');
+  });
+
+  $cellButtonGroup.on('click', '.button', function () {
+      var index = $(this).index();
+      $carousel.flickity('select', index);
+  });
+
+  $(' .button--previous').on('click', function () {
+      $carousel.flickity('previous');
+  });
+  $(' .button--next').on('click', function () {
+      $carousel.flickity('next');
   });
