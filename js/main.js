6605fcf132137b83136a1e2a4fc437b8610a2c2d
Dulce Perez <dulcefrancelispc@gmail.com>
2019-08-26 02:05:09 -0400

    add new views and fixed details

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 //fixid menu on scroll
-$(window).on('scroll', function() {
+$(window).on('scroll', function () {
     var scroll = $(window).scrollTop();
     if (scroll <= 153) {
         $('.main-nav').removeClass('fixed');
@@ -15,27 +15,68 @@ var $carousel = $('.homeslide').flickity({
     cellAlign: 'left'
 });
 // Flickity instance
-    var flkty = $carousel.data('flickity');
-  // elements
-  var $cellButtonGroup = $('.button-group--cells');
-  var $cellButtons = $cellButtonGroup.find('.button');
+var flkty = $carousel.data('flickity');
+// elements
+var $cellButtonGroup = $('.button-group--cells');
+var $cellButtons = $cellButtonGroup.find('.button');
 
-  // update selected cellButtons
-  $carousel.on('select.flickity', function () {
-      $cellButtons.filter('.is-selected')
-          .removeClass('is-selected');
-      $cellButtons.eq(flkty.selectedIndex)
-          .addClass('is-selected');
-  });
+// update selected cellButtons
+$carousel.on('select.flickity', function () {
+    $cellButtons.filter('.is-selected')
+        .removeClass('is-selected');
+    $cellButtons.eq(flkty.selectedIndex)
+        .addClass('is-selected');
+});
 
-  $cellButtonGroup.on('click', '.button', function () {
-      var index = $(this).index();
-      $carousel.flickity('select', index);
-  });
+$cellButtonGroup.on('click', '.button', function () {
+    var index = $(this).index();
+    $carousel.flickity('select', index);
+});
 
-  $(' .button--previous').on('click', function () {
-      $carousel.flickity('previous');
-  });
-  $(' .button--next').on('click', function () {
-      $carousel.flickity('next');
-  });
+$(' .button--previous').on('click', function () {
+    $carousel.flickity('previous');
+});
+$(' .button--next').on('click', function () {
+    $carousel.flickity('next');
+});
+
+//MODAL FOOTER
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    captionText.innerHTML = dots[slideIndex - 1].alt;
+}
