d14b10d6a04d525140b11d05d1716e8d82c64b94
Dulce Perez <d.perez@kation.com.do>
2020-08-30 01:02:24 -0400

    Fix some csd details and html issues

--- js/main.js
+++ js/main.js
@@ -13,32 +13,7 @@ var $carousel = $('.homeslide').flickity({
     prevNextButtons: false,
     pageDots: false,
     cellAlign: 'left',
-    autoPlay: true
-});
-// Flickity instance
-var flkty = $carousel.data('flickity');
-// elements
-var $cellButtonGroup = $('.button-group--cells');
-var $cellButtons = $cellButtonGroup.find('.button');
-
-// update selected cellButtons
-$carousel.on('select.flickity', function () {
-    $cellButtons.filter('.is-selected')
-        .removeClass('is-selected');
-    $cellButtons.eq(flkty.selectedIndex)
-        .addClass('is-selected');
-});
-
-$cellButtonGroup.on('click', '.button', function () {
-    var index = $(this).index();
-    $carousel.flickity('select', index);
-});
-
-$(' .button--previous').on('click', function () {
-    $carousel.flickity('previous');
-});
-$(' .button--next').on('click', function () {
-    $carousel.flickity('next');
+    // autoPlay: true
 });
 
 //MODAL FOOTER
@@ -50,6 +25,7 @@ function closeModal() {
     document.getElementById("myModal").style.display = "none";
 }
 
+//modal footer slide
 var slideIndex = 1;
 showSlides(slideIndex);
 
